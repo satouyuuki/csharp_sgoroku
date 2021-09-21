1507d610e25ed5f033277a27014fdbcafe67a7b2
satoyuki <satoyuki@satoyukinoMacBook-Air.local>
2021-09-21 23:41:06 +0900

    add dice test

---
+++ SugorokuTest/DiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace Sugoroku.Tests
+{
+    public class DiceTests
+    {
+        /// <summary>
+        /// サイコロのインスタンス
+        /// </summary>
+        Dice dice;
+
+        public DiceTests()
+        {
+            dice = new Dice();
+        }
+
+        [Fact]
+        public void Number_Test()
+        {
+            Assert.Equal(6, dice.Number);
+        }
+
+        [Fact]
+        public void GetDiceNumber_Test()
+        {
+            dice.GetDiceNumber();
+            Assert.InRange(dice.CurrentRoll, 1, 6);
+        }
+    }
+}
